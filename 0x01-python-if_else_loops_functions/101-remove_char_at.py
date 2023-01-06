3a826ad8d1c8ebf9e414303919649a6ec97c3e6e
Shalyne-K <shalyne.kerubo@gmail.com>
2023-01-05 19:14:16 -0800

    Function that creates a copy of the string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if (a < b):
-        return (c)
-    elif (c > b):
-        return (a + b)
-    else:
-        return (a * b - c)
+def remove_char_at(str, n):
+    s = ""
+    for i in range(len(str)):
+        if i != n:
+            s = s + str[i]
+    return (s)
