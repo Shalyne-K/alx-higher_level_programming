9616a07b43dd5e6acaf7388f3ca3b577741fdd31
Shalyne-K <shalyne.kerubo@gmail.com>
2023-01-11 18:14:35 -0800

    Weighted average

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,11 +17,9 @@ def roman_to_int(roman_string):
 
     roman_num = 0
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-    if (i != (len(roman_string) - 1) and
-            roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-        roman_num += roman_dict[roman_string[i]] * -1
+        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
+            roman_num += roman_dict[roman_string[i]] - 2 * \
+                roman_dict[roman_string[i - 1]]
     else:
         roman_num += roman_dict[roman_string[i]]
     return (roman_num)
